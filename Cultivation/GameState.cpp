f0008f80ac55448c8baf1e33dd2b471b57ef6366
Odin <andreas.kammerloher@arcor.de>
2017-01-02 22:09:55 +0100

    removed outdated comments about the currentAnim bug, moved building to be inside a tile properly

--- Cultivation/GameState.cpp
+++ Cultivation/GameState.cpp
@@ -14,9 +14,9 @@ GameState::GameState()
 	for (int i = 0; i < 20; i++) {
 		std::vector<Tile> tileRow;
 		for (int j = 0; j < 20; j++) {
-			if (j % 2 == 0) {  // i = 2: all not set, 1: set, 0: set, 3: set, 4: set, 5 set, 16: set
+			if (j % 2 == 0) {
 				if (i > 5 && i < 10 && j < 5) {
-					tileRow.push_back(Tile(i * w, j * h * globals::tileOffsetMultY, TileTypes::Type::mountain, 1)); // type here determines right anims
+					tileRow.push_back(Tile(i * w, j * h * globals::tileOffsetMultY, TileTypes::Type::mountain, 1));
 				}
 				else if (i > 0 && i < 4 && j < 5 && j > 1) {
 					tileRow.push_back(Tile(i * w, j * h * globals::tileOffsetMultY, TileTypes::Type::hill, 1));
@@ -26,7 +26,7 @@ GameState::GameState()
 					tileRow.push_back(Tile(i * w, j * h * globals::tileOffsetMultY, TileTypes::Type::water, 4));
 				}
 				else {
-					tileRow.push_back(Tile(i * w, j * h * globals::tileOffsetMultY, TileTypes::Type::plains, 1)); // type here determines all other anims
+					tileRow.push_back(Tile(i * w, j * h * globals::tileOffsetMultY, TileTypes::Type::plains, 1));
 				}
 			}
 			else {
@@ -39,7 +39,6 @@ GameState::GameState()
 	for (auto& tileRow : tiles) {
 		for (auto& tile : tileRow) {
 			tile.findNeighbours(tiles);
-			//tile.currentAnim = &tile.animations.at(AnimationType::idle);  // if we don't do this tile animations are set to "Hexagon" in cases where Hexagon isn't even in their animations map :x
 		}
 	}
 
@@ -49,11 +48,10 @@ GameState::GameState()
 	}
 
 	// create a building
-	Building b(2 * w, 1 * h * globals::tileOffsetMultY, "Building");
+	Building b(2 * w + w * globals::tileOffsetMultX, 1 * h * globals::tileOffsetMultY, "Building");
 	buildings.push_back(std::move(b));
 
 	for (auto& building : buildings) {
-		//building.currentAnim = &building.animations.at(AnimationType::idle);
 	}
 }
 
